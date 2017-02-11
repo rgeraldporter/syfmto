5172854499fcd0afa18ec0890c1161c8590f1a8a
Rob Porter <rob@weeverapps.com>
2017-02-11 10:15:49 -0500

    Prettifying

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import * as R from 'ramda';
 
-const Validation = x =>
-({
+const Validation = x => ({
     map: f => Validation(f(x)),
     fold: f => f(x),
     liftAN: (n, f) => R.curryN(
         n,
-        (...validations) => R.reduce(
-            (a, b) => a.ap(b),
-            R.head(validations).map(R.curry(f)),
-            R.tail(validations)
-        )
+        (...validations) => R.reduce((a, b) => a.ap(b), R.head(validations).map(R.curry(f)), R.tail(validations))
     )
 });
 
-// Success is not a monoid
 const Success = x => ({
     map: f => Success(f(x)),
     chain: f => f(x),
@@ -24,14 +18,12 @@ const Success = x => ({
     isSuccess: true
 });
 
-// Failure is a monoid?
 const Failure = x => ({
     map: f => Failure(x),
     chain: f => Failure(x),
     fold: (f, g) => g(x),
     inspect: () => `Failure(${x})`,
-    concat: o =>
-        o.fold(null, r => Failure(x.concat(r))),
+    concat: o => o.fold(null, r => Failure(x.concat(r))),
     ap: y => y.isSuccess ? Failure(x) : y.concat(Failure(x)),
     isSuccess: false
 });
--- src/index.spec.js
+++ src/index.spec.js
@@ -5,62 +5,46 @@ const Success = Valdation.Success;
 const Failure = Valdation.Failure;
 
 describe('The library', () => {
+    const validPath = response => R.path([ 'data' ], response) === undefined
+        ? Failure([ 'Object has bad path' ])
+        : Success(response);
 
-    const validPath = response =>
-        R.path(['data'], response) === undefined ?
-            Failure(['Object has bad path']) :
-            Success(response);
-
-    const validFormat = response =>
-        R.path(['data'], response) > 0 ?
-            Success(response) :
-            Failure(['Value is too low!']);
+    const validFormat = response => R.path([ 'data' ], response) > 0
+        ? Success(response)
+        : Failure([ 'Value is too low!' ]);
 
     it('should return a success correctly', () => {
-
         const response = { data: 1 };
-        const success = R.curryN( 2, () => response.data );
+        const success = R.curryN(2, () => response.data);
 
-        const result = Success(success)
-            .ap(validFormat(response))
-            .ap(validPath(response));
+        const result = Success(success).ap(validFormat(response)).ap(validPath(response));
 
         expect(result.inspect()).toBe('Success(1)');
     });
 
     it('should return two failures correctly', () => {
-
         const response = { datas: 1 };
-        const success = R.curryN( 2, () => response.data );
+        const success = R.curryN(2, () => response.data);
 
-        const result = Success(success)
-            .ap(validFormat(response))
-            .ap(validPath(response));
+        const result = Success(success).ap(validFormat(response)).ap(validPath(response));
 
         expect(result.inspect()).toBe('Failure(Object has bad path,Value is too low!)');
     });
 
     it('should return one failure correctly', () => {
-
         const response = { data: -1 };
-        const success = R.curryN( 2, () => response.data );
+        const success = R.curryN(2, () => response.data);
 
-        const result = Success(success)
-            .ap(validFormat(response))
-            .ap(validPath(response));
+        const result = Success(success).ap(validFormat(response)).ap(validPath(response));
 
         expect(result.inspect()).toBe('Failure(Value is too low!)');
     });
 
-
     it('should return one failure correctly no matter what order it is checked', () => {
-
         const response = { data: -1 };
-        const success = R.curryN( 2, () => response.data );
+        const success = R.curryN(2, () => response.data);
 
-        const result = Success(success)
-            .ap(validPath(response))
-            .ap(validFormat(response));
+        const result = Success(success).ap(validPath(response)).ap(validFormat(response));
 
         expect(result.inspect()).toBe('Failure(Value is too low!)');
     });
